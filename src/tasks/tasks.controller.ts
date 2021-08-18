1195b1249005f5cde7bfb70a03ea2f9ca0dd5351
Łukasz Barylak <lbarylak@nertigo.com>
2021-08-18 23:13:18 +0200

    done validation for filters and search

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,6 @@
 import {Body, Controller, Delete, Get, Param, Patch, Post, Query} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
-import {Task, TaskStatus} from "./task.model";
+import {Task} from "./task.model";
 import {CreateTaskDto} from "./dto/create-task.dto";
 import {GetTasksFilterDto} from "./dto/get-tasks-filter.dto";
 import {UpdateTaskStatusDto} from "./dto/update-task-status.dto";
