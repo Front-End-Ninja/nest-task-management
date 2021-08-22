4d4f6c472ebddf9f15ce303c19d650faf465e060
Łukasz Barylak <lbarylak@nertigo.com>
2021-08-22 20:57:01 +0200

    prepared backend for deploy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,18 +6,24 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {JwtModule} from "@nestjs/jwt";
 import {PassportModule} from "@nestjs/passport";
 import { JwtStrategy } from './jwt.strategy';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [
-      PassportModule.register({defaultStrategy: 'jwt'}),
-      JwtModule.register({
-        secret: 'topSecret51',
-        signOptions: {
-          expiresIn: 3600,
-        }
-      }),
-      TypeOrmModule.forFeature([UsersRepository])
-  ],
+    imports: [
+        ConfigModule,
+        PassportModule.register({defaultStrategy: 'jwt'}),
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async (configService: ConfigService) => ({
+                secret: configService.get('JWT_SECRET'),
+                signOptions: {
+                    expiresIn: 3600,
+                }
+            })
+        }),
+        TypeOrmModule.forFeature([UsersRepository])
+    ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
     exports: [JwtStrategy, PassportModule],
